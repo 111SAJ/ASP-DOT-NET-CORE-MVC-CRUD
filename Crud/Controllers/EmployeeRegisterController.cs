e3dc29e982b6e86107f0cf9e053b12505e0fef60
111SAJ <147030794+111SAJ@users.noreply.github.com>
2024-04-23 23:23:45 +0530

    Initial Commit
    CRUD Working - Simple

---
+++ Crud/Controllers/EmployeeRegisterController.cs
@@ -0,0 +1,99 @@
+﻿using Crud.Data;
+using Crud.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Crud.Controllers
+{
+    public class EmployeeRegisterController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EmployeeRegisterController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //List
+        public IActionResult Index()
+        {
+            //var EmployeeList = _context.EmployeeRegister.OrderByDescending(e => e.EmployeeId).ToList();
+            var EmployeeList = _context.EmployeeRegister.ToList();
+            return View(EmployeeList);
+        }
+
+        //Create
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var newEmployee = new EmployeeRegister();
+            return View(newEmployee);
+        }
+
+        [HttpPost]
+        public IActionResult Create(EmployeeRegister employeeRegister)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.EmployeeRegister.Add(employeeRegister);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(employeeRegister);
+        }
+
+        //Update
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var employee = _context.EmployeeRegister.Find(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, EmployeeRegister employeeRegister)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Update(employeeRegister);
+                _context.SaveChanges();
+
+                return RedirectToAction(("Index"));
+            }
+            return View(employeeRegister);
+        }
+
+        //Delete
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var employee = _context.EmployeeRegister.Find(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var employee = _context.EmployeeRegister.Find(id);
+
+            if (employee != null)
+            {
+                _context.EmployeeRegister.Remove(employee);
+                _context.SaveChanges();
+            }
+            return RedirectToAction(("Index"));
+        }
+
+    }
+}
---
+++ Crud/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Crud.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Crud.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) 
+        {
+        
+        }
+        public DbSet<EmployeeRegister> EmployeeRegister { get; set; }
+
+    }
+}
